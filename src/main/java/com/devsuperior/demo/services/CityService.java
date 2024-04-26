363e5784001d054dc4528d9f3145622c8bce1d1c
Alexandre Ferreira <alexandrejfneto@gmail.com>
2024-04-26 20:51:47 -0300

    FindALL and Insert City Solution Implemented

--- src/main/java/com/devsuperior/demo/services/CityService.java
+++ src/main/java/com/devsuperior/demo/services/CityService.java
@@ -1,8 +1,38 @@
 package com.devsuperior.demo.services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.demo.dto.CityDTO;
+import com.devsuperior.demo.entities.City;
+import com.devsuperior.demo.repositories.CityRepository;
 
 @Service
 public class CityService {
+	
+	@Autowired
+	private CityRepository repository;
+
+	@Transactional (readOnly = true)
+	public List<CityDTO> findAll() {
+		//PageRequest pageRequest = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), Sort.by("name"));
+		List<City> list = repository.findAll(Sort.by("name"));
+		return list.stream().map(x -> new CityDTO(x)).collect(Collectors.toList());
+	}
+
+	@Transactional
+	public CityDTO insert(CityDTO cityDTO) {
+		City city = new City();
+		city.setName(cityDTO.getName());
+		city = repository.save(city);
+		return new CityDTO(city);
+	}
+	
+	
 
 }
