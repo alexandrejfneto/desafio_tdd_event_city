cfa2eea4225750684776914ea0ac0283788b271f
Alexandre Ferreira <alexandrejfneto@gmail.com>
2024-04-26 21:03:08 -0300

    Delete City Solution Implemented

--- src/main/java/com/devsuperior/demo/controllers/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/devsuperior/demo/controllers/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.devsuperior.demo.services.exceptions.DatabaseException;
 import com.devsuperior.demo.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -17,6 +18,17 @@ public class ResourceExceptionHandler {
 	@ExceptionHandler (ResourceNotFoundException.class)
 	public ResponseEntity<StandardError> entityNotFound (ResourceNotFoundException e, HttpServletRequest request){
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError error = new StandardError();
+		error.setTimestamp(Instant.now());
+		error.setStatus(status.value());
+		error.setError(e.getMessage());
+		error.setPath(request.getRequestURI());
+		return ResponseEntity.status(status).body(error);
+	}
+	
+	@ExceptionHandler (DatabaseException.class)
+	public ResponseEntity<StandardError> database (DatabaseException e, HttpServletRequest request){
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError error = new StandardError();
 		error.setTimestamp(Instant.now());
 		error.setStatus(status.value());
